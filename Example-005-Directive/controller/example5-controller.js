3c3919ecaa7a8eecacb07686e7630b9a74f33277
arunbaiswal <arunbaiswal@gmail.com>
2015-12-12 03:19:52 +0530

    Update example5-controller.js

--- Example-005-Directive/controller/example5-controller.js
+++ Example-005-Directive/controller/example5-controller.js
@@ -5,18 +5,17 @@ var app = angular.module('example5', []);
 
 // Directive name should be lower case
 
+app.directive('restricta', function () {
+    return {
+        restrict : 'A', // default restrict
+        template: "<div style=\"color:green\">2. Restrict to Attribute</div>"
+    }
+});
+
 app.directive('restricte', function () {
     return {
         restrict : 'E',
         template: "<div style=\"color:red\">1. Restrict to Element</div>"
-    }
-});
-
-
-app.directive('restricta', function () {
-    return {
-        restrict : 'A',
-        template: "<div style=\"color:green\">2. Restrict to Attribute</div>"
     }
 });
 
